405dbe5fd367c409d4d02bc45579714497256398
Bill_Billions <williamrobertson28@gmail.com>
2020-03-04 09:21:12 -0700

    idk if this will workout good

---
+++ .dev/createaudioplayers.bat
@@ -0,0 +1,68 @@
+<doctype html><html lang='en-us' />
+<head>
+<meta http-equiv='xmlns' content='lang:xml' />
+<meta charet='utf-8' />
+<meta name='viewport' content='width=device-width' />
+<script type='application/javascript'>
+set song=;
+
+set d=
+
+if "./musicindex.html" echo '<!-- title musicindex.html -->'>musicindex.html
+	for %s in (dir /s /b) DO
+		echo '<a href="{0}">{0}</a><br />' >musicindex.html
+		
+    echo is not working.???
+	pause >nul
+
+pause >nul
+</script>
+</head>
+<body>
+  <div class='wrapper'>
+    <header>
+	  <div class='text inner'>
+	    <h2>Create Audio Player</h2>
+	    <p>Drop this file on inside the music folder you wish to have you wish to have the contents of indexed into JSON format for the audio player</p>
+	  </div>
+	</header>
+	<nav>
+	  <ul>
+	    <li><a href='#'>Nav Item</a></li>
+	    <li><a href='#'>Nav Item</a></li>
+	    <li><a href='#'>Nav Item</a></li>
+	    <li><a href='#'>Nav Item</a></li>
+	    <li><a href='#'>Nav Item</a></li>
+	  </ul>
+	</nav>
+	<article>
+	  <h1>Headline</h1>
+	  <p><b>Tagline</b></p>
+	  <section>
+		</h3>title</h3>
+		<p>Description</p>
+	  </section>
+	  <section>
+		</h3>title</h3>
+		<p>Description</p>
+	  </section>
+	</article>
+	<article>
+	  <h1>Headline</h1>
+	  <p><b>Tagline</b></p>
+	  <section>
+		</h3>title</h3>
+		<p>Description</p>
+	  </section>
+	  <section>
+		</h3>title</h3>
+		<p>Description</p>
+	  </section>
+	</article>
+  </div>
+  <script type='text/javascript'>
+    function createAudioPlayer(dir) {
+	    for
+	}
+  </script>
+</body></html>
