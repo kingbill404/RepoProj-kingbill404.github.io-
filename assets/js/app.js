bee8bd496a327075148afcfc54122222ad5add76
Bill_Billions <williamrobertson28@gmail.com>
2020-03-21 21:14:40 -0600

    cool

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,70 @@
 alert("hi");
+
+
+$(function onLoad() {
+
+    const q = 0;
+
+    alert('Welcome to my Repository, take a minute to sign my guestBook and while you\'re there\n' > +
+        'leave me a message' + q);
+});
+
+q++;
+
+var elements = [];
+
+for (i = 0, $element in document.html) {
+    document.getElementByClass(".array");
+    inner.html = document.Write(elements[i] + " status: Indexed");
+}
+return $element;
+
+);
+
+var p = [];
+for (var i in window.listen) {
+
+    p.push(i);
+}
+
+alert(p.join('window.listen\'s properties'));
+
+alert(p.join("\gn"));
+/* Assigning functions: 
+    Used to call and or provide the context (object) for a 
+	function that is dependant, Often, functions are assigned 
+	to objects and access object members (properties)using the [this] keyword. 
+	applies object to function and calls the function, object. 
+	This only applys for that call /Heres the stntax: 
+	*/
+
+// Heres the Syntax
+functionName.call(assignedObject, passedParams);
+
+
+//apply is identical, but takes an array of params 
+functionName.apply(assignedObject, [passedParams]);
+
+//permanent assignment of an object to a function 
+functionName.bind(assignedObject);
+
+//object assigned to all subsequent function calls without specifying it 
+functionName(passedParams);
+//More at SyntaxDB 
+};
+
+function splashDamage(node, x, y, damage) {
+    if (node.classList.contains('tower') && // checking the 'class' attribute
+        node.dataset.x == x && // reading the 'data-x' attribute
+        node.dataset.y == y) { // reading the 'data-y' attribute
+        var hp = parseInt(node.dataset.hp); // reading the 'data-hp' attribute
+        hp = hp - damage;
+        if (hp < 0) {
+            hp = 0;
+            node.dataset.ai = 'dead'; // setting the 'data-ai' attribute
+            delete node.dataset.ability; // removing the 'data-ability' attribute
+        }
+        node.dataset.hp = hp; // setting the 'data-hp' attribute
+    }
+});
+onClick(mouse.x, mouse.y) { spalshDamage(0, mouse.y, mouse.x, --damage); }
