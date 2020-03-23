46e12d0df47861a0787122a126686b0dc923ce31
Bill_Billions <williamrobertson28@gmail.com>
2020-03-23 07:01:52 -0600

    poo poop

--- assets/js/sketch.js
+++ assets/js/sketch.js
@@ -9,45 +9,45 @@ let scl = 10;
 let food;
 
 function setup() {
-  createCanvas(800, 400);
-  s = new Snake();
-  frameRate(10);
-  pickLocation();
+    createCanvas(1200, 950);
+    s = new Snake();
+    frameRate(12);
+    pickLocation();
 }
 
 function pickLocation() {
-  let cols = floor(width / scl);
-  let rows = floor(height / scl);
-  food = createVector(floor(random(cols)), floor(random(rows)));
-  food.mult(scl);
+    let cols = floor(width / scl);
+    let rows = floor(height / scl);
+    food = createVector(floor(random(cols)), floor(random(rows)));
+    food.mult(scl);
 }
 
 function mousePressed() {
-  s.total++;
+    s.total++;
 }
 
 // Draw The Game Loop On The Canvas
 function draw() {
-  background(51);
-  if (s.eat(food)) {
-    pickLocation();
-  }
-  s.death();
-  s.update();
-  s.show();
-  fill(20, 160, 250);
-  rect(food.x, food.y, scl, scl);
+    background(32);
+    if (s.eat(food)) {
+        pickLocation();
+    }
+    s.death();
+    s.update();
+    s.show();
+    fill(20, 260, 250);
+    rect(food.x, food.y, scl, scl);
 }
 
 // Keyboard Controls:
 function keyPressed() {
-  if (keyCode === UP_ARROW) {
-    s.dir(0, -1);
-  } else if (keyCode === DOWN_ARROW) {
-    s.dir(0, 1);
-  } else if (keyCode === RIGHT_ARROW) {
-    s.dir(1, 0);
-  } else if (keyCode === LEFT_ARROW) {
-    s.dir(-1, 0);
-  }
+    if (keyCode === UP_ARROW) {
+        s.dir(0, -1);
+    } else if (keyCode === DOWN_ARROW) {
+        s.dir(0, 1);
+    } else if (keyCode === RIGHT_ARROW) {
+        s.dir(1, 0);
+    } else if (keyCode === LEFT_ARROW) {
+        s.dir(-1, 0);
+    }
 }
